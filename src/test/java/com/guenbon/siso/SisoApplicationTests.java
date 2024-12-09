047e688d960fdb6a621e86aa6f266a6e27142b9a
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-09 22:48:57 +0900

    chore: 초기 설정

---
+++ src/test/java/com/guenbon/siso/SisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guenbon.siso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
