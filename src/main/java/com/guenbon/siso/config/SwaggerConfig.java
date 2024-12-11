272a3039f10fcfe4a7564960823b40c01d1d62be
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-12 08:47:58 +0900

    chore: swagger 설정 추가

---
+++ src/main/java/com/guenbon/siso/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.guenbon.siso.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("siso Swagger")
+                .description("siso api 명세")
+                .version("1.0.0");
+    }
+}
