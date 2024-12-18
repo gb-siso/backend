0ff1f8bc9069ee5282f63477a5a9d84c629a137f
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-18 12:17:39 +0900

    docs: swagger jwt 설정 추가

--- src/main/java/com/guenbon/siso/config/SwaggerConfig.java
+++ src/main/java/com/guenbon/siso/config/SwaggerConfig.java
@@ -2,9 +2,10 @@ package com.guenbon.siso.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,8 +13,22 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("accessToken", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                        .addSecuritySchemes("refreshToken", new SecurityScheme()
+                                .name("refreshToken")
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.COOKIE)
+                                .description("Refresh token stored in cookies.")
+                        )
+                )
                 .info(apiInfo());
+
     }
 
     private Info apiInfo() {
