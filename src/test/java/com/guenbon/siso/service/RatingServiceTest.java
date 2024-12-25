c8900a0a4dc44c83db5c416dde823b47cae3d35f
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-25 20:19:56 +0900

    test: RatingServiceTest create 중복 예외 테스트 작성

--- src/test/java/com/guenbon/siso/service/RatingServiceTest.java
+++ src/test/java/com/guenbon/siso/service/RatingServiceTest.java
@@ -4,8 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import com.guenbon.siso.entity.Congressman;
+import com.guenbon.siso.entity.Member;
+import com.guenbon.siso.exception.BadRequestException;
+import com.guenbon.siso.exception.errorCode.RatingErrorCode;
 import com.guenbon.siso.repository.RatingRepository;
-import java.util.Optional;
+import com.guenbon.siso.support.fixture.CongressmanFixture;
+import com.guenbon.siso.support.fixture.MemberFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,6 +25,10 @@ class RatingServiceTest {
     RatingService ratingService;
     @Mock
     RatingRepository ratingRepository;
+    @Mock
+    MemberService memberService;
+    @Mock
+    CongressmanService congressmanService;
 
     @Test
     void ratingService_null_아님() {
@@ -27,15 +36,23 @@ class RatingServiceTest {
     }
 
     @Test
-    @DisplayName("중복되는 memberId와 congressmanId에 대해 create 메서드 호출 시 DuplicateRatingException을 던진다")
+    @DisplayName("중복되는 memberId와 congressmanId에 대해 create 메서드 호출 시 BadRequestException을 던진다")
     void create_duplicate_DuplicateRatingException() {
         // given
-        final Long memberId = 1L;
-        final Long congressmanId = 1L;
-
-        when(ratingRepository.findByMemberIdAndCongressmanId(memberId,congressmanId)).thenReturn(Optional.empty());
+        final Member 장몽이 = MemberFixture.builder()
+                .setId(1L)
+                .setNickname("장몽이")
+                .build();
+        final Congressman 이준석 = CongressmanFixture.builder()
+                .setId(1L)
+                .setName("이준석")
+                .build();
+        when(ratingRepository.existsByMemberAndCongressman(장몽이, 이준석)).thenReturn(true);
+        when(memberService.findById(장몽이.getId())).thenReturn(장몽이);
+        when(congressmanService.findById(이준석.getId())).thenReturn(이준석);
 
         // when, then
-        assertThrows(DuplicateRatingException.class, ratingService.create(memberId, congressmanId));
+        assertThrows(BadRequestException.class, () -> ratingService.create(장몽이.getId(), 이준석.getId()),
+                RatingErrorCode.DUPLICATED.getMessage());
     }
 }
