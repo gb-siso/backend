1755ea4ebcd762b31f054892e7b28901d940492b
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-27 06:51:00 +0900

    test: RatingControllerTest create 회원, 국회의원 존재 X 테스트 , 정상 테스트 수정
    - 컨트롤러에서는 RatingService에서만 의존하고 RatingService에서 다른 서비스에 의존하는 구조로 바꾸기 위해 테스트 수정 (의존 서비스 및 stubbing 수정)
    - 정상 테스트 : stubbing 수정, 리다이렉션 추가
    - 실패 테스트 : stubbing 수정

--- src/test/java/com/guenbon/siso/controller/RatingControllerTest.java
+++ src/test/java/com/guenbon/siso/controller/RatingControllerTest.java
@@ -1,31 +1,36 @@
 package com.guenbon.siso.controller;
 
+import static com.guenbon.siso.exception.errorCode.RatingErrorCode.DUPLICATED;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guenbon.siso.dto.rating.request.RatingWriteDTO;
 import com.guenbon.siso.entity.Congressman;
 import com.guenbon.siso.entity.Member;
-import com.guenbon.siso.entity.Rating;
 import com.guenbon.siso.exception.BadRequestException;
 import com.guenbon.siso.exception.errorCode.CongressmanErrorCode;
 import com.guenbon.siso.exception.errorCode.MemberErrorCode;
 import com.guenbon.siso.exception.errorCode.RatingErrorCode;
 import com.guenbon.siso.service.AESUtil;
-import com.guenbon.siso.service.CongressmanService;
 import com.guenbon.siso.service.JwtTokenProvider;
-import com.guenbon.siso.service.MemberService;
+import com.guenbon.siso.service.RatingService;
 import com.guenbon.siso.support.fixture.CongressmanFixture;
 import com.guenbon.siso.support.fixture.MemberFixture;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
@@ -34,6 +39,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 @Import({AESUtil.class, JwtTokenProvider.class})
+@Slf4j
 class RatingControllerTest {
 
     @Autowired
@@ -46,10 +52,10 @@ class RatingControllerTest {
     JwtTokenProvider jwtTokenProvider;
 
     @MockitoBean
-    MemberService memberService;
+    RatingService ratingService;
 
-    @MockitoBean
-    CongressmanService congressmanService;
+    @Value("${spring.siso.domain}")
+    String domain;
 
     @Test
     void 빈_주입_화인() {
@@ -61,7 +67,7 @@ class RatingControllerTest {
     }
 
     @Test
-    @DisplayName("이미 존재하는 Rating 작성 요청에 대해 에러 응답을 반환한다")
+    @DisplayName("중복 Rating 작성 요청에 대해 에러 응답을 반환한다")
     void create_duplicate_ErrorResponse() throws Exception {
 
         final Long CONGRESSMAN_ID = 1L;
@@ -77,14 +83,17 @@ class RatingControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
         final String json = objectMapper.writeValueAsString(BAD_REQUEST);
 
+        doThrow(new BadRequestException(RatingErrorCode.DUPLICATED)).when(ratingService)
+                .create(MEMBER_ID, CONGRESSMAN_ID);
+
         mockMvc.perform(post("/api/v1/ratings")
                         .header("accessToken", accessToken)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andDo(print())
                 .andExpect(status().is4xxClientError())
-                .andExpect(jsonPath("$.message").value(RatingErrorCode.DUPLICATED.getMessage()))
-                .andExpect(jsonPath("$.code").value(RatingErrorCode.DUPLICATED.name()))
+                .andExpect(jsonPath("$.message").value(DUPLICATED.getMessage()))
+                .andExpect(jsonPath("$.code").value(DUPLICATED.name()))
                 .andReturn();
     }
 
@@ -104,8 +113,8 @@ class RatingControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
         final String json = objectMapper.writeValueAsString(BAD_REQUEST);
 
-        when(memberService.findById(INVALID_MEMBER_ID))
-                .thenThrow(new BadRequestException(MemberErrorCode.NOT_EXISTS));
+        doThrow(new BadRequestException(MemberErrorCode.NOT_EXISTS)).when(ratingService)
+                .create(INVALID_MEMBER_ID, CONGRESSMAN_ID);
 
         mockMvc.perform(post("/api/v1/ratings")
                         .header("accessToken", accessToken)
@@ -134,8 +143,8 @@ class RatingControllerTest {
         ObjectMapper objectMapper = new ObjectMapper();
         final String json = objectMapper.writeValueAsString(BAD_REQUEST);
 
-        when(congressmanService.findById(INVALID_CONGRESSMAN_ID))
-                .thenThrow(new BadRequestException(CongressmanErrorCode.NOT_EXISTS));
+        doThrow(new BadRequestException(CongressmanErrorCode.NOT_EXISTS)).when(ratingService)
+                .create(MEMBER_ID, INVALID_CONGRESSMAN_ID);
 
         mockMvc.perform(post("/api/v1/ratings")
                         .header("accessToken", accessToken)
@@ -149,6 +158,7 @@ class RatingControllerTest {
     }
 
     @Test
+    @DisplayName("정상 Rating 작성 요청에 대해 리다이렉션한다")
     void create_normalInput_200() throws Exception {
 
         final Member member = MemberFixture.builder()
@@ -168,26 +178,20 @@ class RatingControllerTest {
                 .content("평범한 국회의원")
                 .rating(3.0F).build();
 
-        final Rating expected = Rating.builder()
-                .id(1L)
-                .congressman(congressman)
-                .member(member)
-                .build();
-
         ObjectMapper objectMapper = new ObjectMapper();
         final String json = objectMapper.writeValueAsString(REQUEST);
 
-        when(memberService.findById(member.getId())).thenReturn(member);
-        when(congressmanService.findById(congressman.getId())).thenReturn(congressman);
+        doNothing().when(ratingService).create(member.getId(), congressman.getId());
 
         mockMvc.perform(post("/api/v1/ratings")
                         .header("accessToken", accessToken)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andDo(print())
-                .andExpect(status().is2xxSuccessful())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(domain + "/api/v1/congressionman/" + ENCRYPTED_CONGRESSMAN_ID))
                 .andReturn();
+
+        verify(ratingService, times(1)).create(member.getId(), congressman.getId());
     }
-
-
 }
