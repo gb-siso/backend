32d6867eaba991d45e207764589a2102da487021
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-26 17:04:02 +0900

    test: RatingControllerTest create 존재하지 않는 회원/국회의원에 대해 Rating 작성 시 에러응답 테스트 작성
    - 유효하지 않은 필드만 다를 뿐 같은 테스트라 한번에 작성함

--- src/test/java/com/guenbon/siso/controller/RatingControllerTest.java
+++ src/test/java/com/guenbon/siso/controller/RatingControllerTest.java
@@ -9,6 +9,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.guenbon.siso.dto.rating.request.RatingWriteDTO;
+import com.guenbon.siso.exception.errorCode.CongressmanErrorCode;
+import com.guenbon.siso.exception.errorCode.MemberErrorCode;
 import com.guenbon.siso.exception.errorCode.RatingErrorCode;
 import com.guenbon.siso.service.AESUtil;
 import com.guenbon.siso.service.JwtTokenProvider;
@@ -43,7 +45,7 @@ class RatingControllerTest {
     }
 
     @Test
-    @DisplayName("이미 존재하는 Rating 작성 요청에 대해 에러 응답을 반환한다.")
+    @DisplayName("이미 존재하는 Rating 작성 요청에 대해 에러 응답을 반환한다")
     void create_duplicate_ErrorResponse() throws Exception {
 
         final Long CONGRESSMAN_ID = 1L;
@@ -67,6 +69,60 @@ class RatingControllerTest {
                 .andExpect(status().is4xxClientError())
                 .andExpect(jsonPath("$.message").value(RatingErrorCode.DUPLICATED.getMessage()))
                 .andExpect(jsonPath("$.code").value(RatingErrorCode.DUPLICATED.name()))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원의 Rating 작성 요청에 대해 에러 응답을 반환한다")
+    void create_notExistMember_ErrorResponse() throws Exception {
+        final Long CONGRESSMAN_ID = 1L;
+        final String ENCRYPTED_CONGRESSMAN_ID = aesUtil.encrypt(CONGRESSMAN_ID);
+        final Long INVALID_MEMBER_ID = 10L;
+        final String accessToken = jwtTokenProvider.createAccessToken(INVALID_MEMBER_ID);
+
+        final RatingWriteDTO BAD_REQUEST = RatingWriteDTO.builder()
+                .congressmanId(ENCRYPTED_CONGRESSMAN_ID)
+                .content("평범한 국회의원")
+                .rating(3.0F).build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        final String json = objectMapper.writeValueAsString(BAD_REQUEST);
+
+        mockMvc.perform(post("/api/v1/ratings")
+                        .header("accessToken", accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.message").value(MemberErrorCode.NOT_EXISTS.getMessage()))
+                .andExpect(jsonPath("$.code").value(MemberErrorCode.NOT_EXISTS.name()))
+                .andReturn();
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 국회의원의 Rating 작성 요청에 대해 에러 응답을 반환한다")
+    void create_notExistCongressman_ErrorResponse() throws Exception {
+        final Long INVALID_CONGRESSMAN_ID = 1L;
+        final String ENCRYPTED_CONGRESSMAN_ID = aesUtil.encrypt(INVALID_CONGRESSMAN_ID);
+        final Long MEMBER_ID = 10L;
+        final String accessToken = jwtTokenProvider.createAccessToken(MEMBER_ID);
+
+        final RatingWriteDTO BAD_REQUEST = RatingWriteDTO.builder()
+                .congressmanId(ENCRYPTED_CONGRESSMAN_ID)
+                .content("평범한 국회의원")
+                .rating(3.0F).build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        final String json = objectMapper.writeValueAsString(BAD_REQUEST);
+
+        mockMvc.perform(post("/api/v1/ratings")
+                        .header("accessToken", accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.message").value(CongressmanErrorCode.NOT_EXISTS.getMessage()))
+                .andExpect(jsonPath("$.code").value(CongressmanErrorCode.NOT_EXISTS.name()))
                 .andReturn();
     }
 }
