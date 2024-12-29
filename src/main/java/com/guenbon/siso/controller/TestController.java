048d1b76c49c87f0dd8f39e227604c93dc552af0
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-29 21:15:19 +0900

    feat: 프론트 테스트 용 컨트롤러 추가

---
+++ src/main/java/com/guenbon/siso/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.guenbon.siso.controller;
+
+import com.guenbon.siso.service.AESUtil;
+import com.guenbon.siso.service.CongressmanService;
+import com.guenbon.siso.service.JwtTokenProvider;
+import com.guenbon.siso.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/test")
+@RequiredArgsConstructor
+public class TestController {
+
+    private final AESUtil aesUtil;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberService memberService;
+    private final CongressmanService congressmanService;
+
+    @GetMapping("/accessToken/{memberId}")
+    public String accessToken(@PathVariable Long memberId) {
+        memberService.findById(memberId);
+        return jwtTokenProvider.createAccessToken(memberId);
+    }
+
+    @GetMapping("/congressman/{congressmanId}")
+    public String congressman(@PathVariable Long congressmanId) {
+        congressmanService.findById(congressmanId);
+        return aesUtil.encrypt(congressmanId);
+    }
+}
