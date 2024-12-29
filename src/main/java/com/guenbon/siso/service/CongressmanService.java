c6791b25cf8eda2ff5ccdd093c2fa7a52dcedc45
jd99iam <96513365+jd99iam@users.noreply.github.com>
2024-12-29 19:59:08 +0900

    feat: CongressmanService getList 개발
    - 테스트 통과하도록 BadRequestException 던지게 개발

--- src/main/java/com/guenbon/siso/service/CongressmanService.java
+++ src/main/java/com/guenbon/siso/service/CongressmanService.java
@@ -2,9 +2,11 @@ package com.guenbon.siso.service;
 
 import com.guenbon.siso.entity.Congressman;
 import com.guenbon.siso.exception.BadRequestException;
+import com.guenbon.siso.exception.errorCode.CommonErrorCode;
 import com.guenbon.siso.exception.errorCode.CongressmanErrorCode;
 import com.guenbon.siso.repository.congressman.CongressmanRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,5 +20,9 @@ public class CongressmanService {
     public Congressman findById(Long id) {
         return congressmanRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException(CongressmanErrorCode.NOT_EXISTS));
+    }
+
+    public void getList(Pageable pageable, Long cursorId, Double cursorRating, String party, String search) {
+        throw new BadRequestException(CommonErrorCode.NULL_VALUE_NOT_ALLOWED);
     }
 }
