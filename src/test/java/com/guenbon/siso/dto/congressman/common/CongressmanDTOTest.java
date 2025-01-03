c37d72cfd37aa615b0876b1b1ab73802def2a550
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-03 09:13:40 +0900

    test: CongressmanDTOTest of 예외 테스트 수정
    - of 메서드에 암호화된 congressmanId도 파라미터로 받아야 해서 변경
    - congressmanId가 null 인 경우도 추가

--- src/test/java/com/guenbon/siso/dto/congressman/common/CongressmanDTOTest.java
+++ src/test/java/com/guenbon/siso/dto/congressman/common/CongressmanDTOTest.java
@@ -7,25 +7,47 @@ import com.guenbon.siso.exception.InternalServerException;
 import com.guenbon.siso.exception.errorCode.CommonErrorCode;
 import java.util.List;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class CongressmanDTOTest {
 
+    @DisplayName("of 메서드에 유효하지 않은 null 형태 파라미터에 대해 InternalServerException을 던지며 에러코드는 CommonErrorCode이다")
     @ParameterizedTest
     @MethodSource("provideOfNullParameters")
-    void of_nullParameters_InternalServerException(final CongressmanGetListDTO congressmanGetListDTO,
-                                                   final List<String> memberImages) {
+    void of_nullParameters_InternalServerException(
+            final String congressmanId,
+            final CongressmanGetListDTO congressmanGetListDTO,
+            final List<String> memberImages) {
         // given : parameters
         // when, then
-        assertThrows(InternalServerException.class, () -> CongressmanDto.of(congressmanGetListDTO, memberImages),
+        assertThrows(InternalServerException.class,
+                () -> CongressmanDto.of(congressmanId, congressmanGetListDTO, memberImages),
                 CommonErrorCode.NULL_VALUE_NOT_ALLOWED.getMessage());
     }
 
     static Stream<Arguments> provideOfNullParameters() {
-        return Stream.of(Arguments.of(null, List.of("image1", "image2", "image3")),
-                Arguments.of(CongressmanGetListDTO.builder().build(), null)
+        Named<CongressmanGetListDTO> congressmanGetListDTO = Named.named("CongressmanGetListDTO",
+                CongressmanGetListDTO.builder().build());
+        Named<String> congressmanId = Named.named("암호화 된 congressmanId", "dkaghghk123dkaghghk123");
+        Named<List<String>> imageUrls = Named.named("imageUrls", List.of("image1", "image2", "image3"));
+        Named<Object> nullValue = Named.named("null value", null);
+
+        return Stream.of(Arguments.of(
+                        congressmanId,
+                        nullValue,
+                        imageUrls),
+                Arguments.of(
+                        congressmanId,
+                        congressmanGetListDTO,
+                        nullValue),
+                Arguments.of(
+                        nullValue,
+                        congressmanGetListDTO,
+                        imageUrls)
         );
     }
 }
