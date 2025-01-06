047d923767c5dd1645e54ddf4faef12e76b73442
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-06 15:57:40 +0900

    feat: QuerydslRatingRepositoryImpl getRecentRatingByCongressmanId 메서드 작성
    - 테스트 통과하도록 작성

--- src/main/java/com/guenbon/siso/repository/rating/QuerydslRatingRepository.java
+++ src/main/java/com/guenbon/siso/repository/rating/QuerydslRatingRepository.java
@@ -1,12 +1,10 @@
 package com.guenbon.siso.repository.rating;
 
 import com.guenbon.siso.entity.Rating;
-import com.guenbon.siso.entity.like.RatingLike;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 
 public interface QuerydslRatingRepository {
-    Optional<List<Rating>> getRecentRatingByCongressmanIdSort(Long congressmanId, Pageable pageable);
-
+    Optional<List<Rating>> getRecentRatingByCongressmanId(Long congressmanId, Pageable pageable);
 }
--- src/main/java/com/guenbon/siso/repository/rating/QuerydslRatingRepositoryImpl.java
+++ src/main/java/com/guenbon/siso/repository/rating/QuerydslRatingRepositoryImpl.java
@@ -1,15 +1,42 @@
 package com.guenbon.siso.repository.rating;
 
+import static com.guenbon.siso.entity.QRating.rating;
+
 import com.guenbon.siso.entity.Rating;
-import com.guenbon.siso.entity.like.RatingLike;
-import com.guenbon.siso.repository.like.QuerydslRatingLikeRepository;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
-public class QuerydslRatingRepositoryImpl implements QuerydslRatingLikeRepository {
+@RequiredArgsConstructor
+public class QuerydslRatingRepositoryImpl implements QuerydslRatingRepository {
 
-    public Optional<List<Rating>> getRecentRatingByCongressmanIdSort(Long congressmanId, Pageable pageable) {
-        return Optional.empty();
+    private final JPAQueryFactory jpaQueryFactory;
+
+
+    public Optional<List<Rating>> getRecentRatingByCongressmanId(Long congressmanId, Pageable pageable) {
+        return Optional.of(jpaQueryFactory.select(rating)
+                .from(rating)
+                .where(rating.congressman.id.eq(congressmanId))
+                .orderBy(getOrderBy(pageable), rating.id.desc())
+                .limit(pageable.getPageSize() + 1)
+                .fetch());
+    }
+
+    private static OrderSpecifier<Integer> getOrderBy(Pageable pageable) {
+
+        for (Sort.Order order : pageable.getSort()) {
+            String property = order.getProperty(); // 정렬 기준 속성
+            if ("like".equalsIgnoreCase(property)) {
+                return rating.ratingLikeList.size().desc();
+            }
+            if ("dislike".equalsIgnoreCase(property)) {
+                return rating.ratingDisLikeList.size().desc();
+            }
+        }
+        return rating.ratingLikeList.size().add(rating.ratingDisLikeList.size()).desc();
     }
 }
