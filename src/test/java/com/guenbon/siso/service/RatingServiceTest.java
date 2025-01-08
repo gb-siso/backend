b62e37b43cccc7310c31d58e22a86891910686b0
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-08 17:31:41 +0900

    test: RatingService getRecentRatingByCongressmanId 테스트 수정
    - getSortedRatingsByCongressmanId 메서드의 페이지별, 다음 스크롤 유무에 따른 올바른 동작을 검증 추가

--- src/test/java/com/guenbon/siso/service/RatingServiceTest.java
+++ src/test/java/com/guenbon/siso/service/RatingServiceTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.guenbon.siso.dto.cursor.count.CountCursor;
+import com.guenbon.siso.dto.cursor.count.DecryptedCountCursor;
 import com.guenbon.siso.dto.rating.response.RatingListDTO;
 import com.guenbon.siso.entity.Congressman;
 import com.guenbon.siso.entity.Member;
@@ -18,6 +21,8 @@ import com.guenbon.siso.exception.errorCode.CommonErrorCode;
 import com.guenbon.siso.exception.errorCode.RatingErrorCode;
 import com.guenbon.siso.repository.rating.RatingRepository;
 import com.guenbon.siso.support.fixture.congressman.CongressmanFixture;
+import com.guenbon.siso.support.fixture.dislike.RatingDislikeFixture;
+import com.guenbon.siso.support.fixture.like.RatingLikeFixture;
 import com.guenbon.siso.support.fixture.member.MemberFixture;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -93,11 +98,12 @@ class RatingServiceTest {
     @Test
     void getRecentRatingByCongressmanId_nullCongressmanId_BadRequestException() {
         // given
-        final PageRequest pageRequest = createPageRequest("topicality");
+        final PageRequest pageable = createPageRequest("topicality", 0);
         when(aesUtil.decrypt(null)).thenThrow(new BadRequestException(CommonErrorCode.NULL_VALUE_NOT_ALLOWED));
 
         // when, then
-        assertThrows(BadRequestException.class, () -> ratingService.validateAndGetRecentRatings(null, pageRequest),
+        assertThrows(BadRequestException.class,
+                () -> ratingService.validateAndGetRecentRatings(null, pageable, null),
                 CommonErrorCode.NULL_VALUE_NOT_ALLOWED.getMessage());
     }
 
@@ -106,12 +112,11 @@ class RatingServiceTest {
     void getRecentRatingByCongressmanId_nullPageRequest_BadRequestException() {
         // given
         final String encryptedCongressmanId = "adksl123897adjadsjfkl";
-        final PageRequest pageRequest = createPageRequest("topicality");
         final Long congressmanId = 3L;
         when(aesUtil.decrypt(encryptedCongressmanId)).thenReturn(congressmanId);
         // when, then
         assertThrows(BadRequestException.class,
-                () -> ratingService.validateAndGetRecentRatings(encryptedCongressmanId, null),
+                () -> ratingService.validateAndGetRecentRatings(encryptedCongressmanId, null, null),
                 CommonErrorCode.NULL_VALUE_NOT_ALLOWED.getMessage());
     }
 
@@ -119,44 +124,76 @@ class RatingServiceTest {
     @Test
     void getRecentRatingByCongressmanId_validInput_RatingListDTO() {
         // given
-        final String encryptedCongressmanId = "adksl123897adjadsjfkl";
-        final PageRequest pageRequest = createPageRequest("topicality");
+        final String encryptedCongressmanId = "encryptedCongressmanId";
+        final PageRequest page1 = createPageRequest("topicality", 0);
+        final PageRequest page2 = createPageRequest("topicality", 1);
         final Long congressmanId = 3L;
 
-        mockBehavior(encryptedCongressmanId, congressmanId, pageRequest);
+        mockBehavior(encryptedCongressmanId, congressmanId, page1, page2);
 
         // when
-        final RatingListDTO result = ratingService.validateAndGetRecentRatings(encryptedCongressmanId, pageRequest);
+        // page1 : 다음 페이지 존재 -> 커서값 존재
+        final RatingListDTO resultPage1 = ratingService.validateAndGetRecentRatings(encryptedCongressmanId, page1,
+                null);
+        // page2 : 다음 페이지 없음 -> 커서값 null
+        final RatingListDTO resultPage2 = ratingService.validateAndGetRecentRatings(encryptedCongressmanId, page2,
+                CountCursor.of("4L", 4));
 
         // then
         assertAll(
                 () -> assertThat(
-                        result.getRatingList().stream().map(rating -> rating.getId()).toList()).containsExactly("3L",
-                        "2L", "1L", "4L"),
+                        resultPage1.getRatingList().stream().map(rating -> rating.getId()).toList())
+                        .containsExactly("3L", "2L", "1L", "4L"),
                 () -> assertThat(
-                        result.getRatingList().stream().map(rating -> rating.getMember().getId())
-                                .toList()).containsExactly("1L", "2L", "3L", "4L"),
+                        resultPage1.getRatingList().stream().map(rating -> rating.getMember().getId()).toList())
+                        .containsExactly("1L", "2L", "3L", "4L"),
+                () -> assertThat(resultPage1.getCountCursor()).usingRecursiveComparison()
+                        .isEqualTo(CountCursor.of("4L", 4)),
                 () -> assertThat(
-                        result.getIdCursor()).isEqualTo("4L"));
+                        resultPage2.getRatingList().stream().map(rating -> rating.getId()).toList())
+                        .containsExactly("3L", "2L", "1L"),
+                () -> assertThat(
+                        resultPage2.getRatingList().stream().map(rating -> rating.getMember().getId()).toList())
+                        .containsExactly("1L", "2L", "3L"),
+                () -> assertThat(
+                        resultPage2.getCountCursor())
+                        .isNull()
+        );
     }
 
-    private void mockBehavior(String encryptedCongressmanId, Long congressmanId, PageRequest pageRequest) {
+    private void mockBehavior(String encryptedCongressmanId, Long congressmanId, PageRequest page1, PageRequest page2) {
+
+        // 공통 mock stubbing
         when(aesUtil.decrypt(encryptedCongressmanId)).thenReturn(congressmanId);
-        when(ratingRepository.getRecentRatingByCongressmanId(congressmanId, pageRequest)).thenReturn(
-                List.of(
-                        Rating.builder().member(MemberFixture.builder().setId(1L).build()).id(3L).build(),
-                        Rating.builder().member(MemberFixture.builder().setId(2L).build()).id(2L).build(),
-                        Rating.builder().member(MemberFixture.builder().setId(3L).build()).id(1L).build(),
-                        Rating.builder().member(MemberFixture.builder().setId(4L).build()).id(4L).build()
-                )
-        );
         when(aesUtil.encrypt(1L)).thenReturn("1L");
         when(aesUtil.encrypt(2L)).thenReturn("2L");
         when(aesUtil.encrypt(3L)).thenReturn("3L");
         when(aesUtil.encrypt(4L)).thenReturn("4L");
+
+        // page 1 에 대해 mock stubbing
+        when(ratingRepository.getSortedRatingsByCongressmanId(congressmanId, page1, null)).thenReturn(
+                List.of(
+                        Rating.builder().member(MemberFixture.fromId(1L)).id(3L).build(),
+                        Rating.builder().member(MemberFixture.fromId(2L)).id(2L).build(),
+                        Rating.builder().member(MemberFixture.fromId(3L)).id(1L).build(),
+                        Rating.builder().member(MemberFixture.fromId(4L)).id(4L).ratingLikeList(
+                                        List.of(RatingLikeFixture.builder().build(), RatingLikeFixture.builder().build()))
+                                .ratingDislikeList(
+                                        List.of(RatingDislikeFixture.builder().build(),
+                                                RatingDislikeFixture.builder().build())).build()));
+
+        // page 2 에 대해 mock stubbing
+        when(aesUtil.decrypt("4L")).thenReturn(4L);
+        when(ratingRepository.getSortedRatingsByCongressmanId(eq(congressmanId), eq(page2),
+                any(DecryptedCountCursor.class)))
+                .thenReturn(
+                        List.of(
+                                Rating.builder().member(MemberFixture.fromId(1L)).id(3L).build(),
+                                Rating.builder().member(MemberFixture.fromId(2L)).id(2L).build(),
+                                Rating.builder().member(MemberFixture.fromId(3L)).id(1L).build()));
     }
 
-    private static PageRequest createPageRequest(String sort) {
-        return PageRequest.of(0, 3, Sort.by(sort).descending());
+    private static PageRequest createPageRequest(final String sort, final Integer page) {
+        return PageRequest.of(page, 3, Sort.by(sort).descending());
     }
 }
