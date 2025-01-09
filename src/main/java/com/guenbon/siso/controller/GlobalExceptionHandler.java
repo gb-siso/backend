4ee2c5cef3e7ce1e3ccfa9d51d551f0c5defdfad
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-10 08:46:51 +0900

    feat: 전역 예외 처리 추가
    - MethodArgumentTypeMismatchException 예외 처리 추가
    - MethodArgumentNotValidException 예외 처리 추가
    - ErrorResponse 에 필드 검증 예외시 응답할 이너 클래스 추가

--- src/main/java/com/guenbon/siso/controller/GlobalExceptionHandler.java
+++ src/main/java/com/guenbon/siso/controller/GlobalExceptionHandler.java
@@ -1,14 +1,48 @@
 package com.guenbon.siso.controller;
 
 import com.guenbon.siso.dto.error.ErrorResponse;
+import com.guenbon.siso.dto.error.ErrorResponse.ValidationError;
 import com.guenbon.siso.exception.CustomException;
+import com.guenbon.siso.exception.InternalServerException;
+import com.guenbon.siso.exception.errorCode.CommonErrorCode;
 import com.guenbon.siso.exception.errorCode.ErrorCode;
+import com.guenbon.siso.exception.errorCode.InternalServerErrorCode;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+@Slf4j
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // 컨트롤러 @RequestParam 타입 불일치 예외 처리
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e) {
+        return handleExceptionInternal(e);
+    }
+
+    private ResponseEntity<ErrorResponse> handleExceptionInternal(MethodArgumentTypeMismatchException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(makeErrorResponse(CommonErrorCode.TYPE_MISMATCH));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAllException(Exception e) {
+        e.printStackTrace();
+        return handleExceptionInternal(new InternalServerException(InternalServerErrorCode.INTERNAL_SERVER_ERROR));
+    }
+
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         return handleExceptionInternal(e);
@@ -17,7 +51,29 @@ public class GlobalExceptionHandler {
     private ResponseEntity<ErrorResponse> handleExceptionInternal(CustomException e) {
         return ResponseEntity.status(e.getHttpStatus()).body(makeErrorResponse(e.getErrorCode()));
     }
+
     private ErrorResponse makeErrorResponse(ErrorCode errorCode) {
         return ErrorResponse.builder().code(errorCode.name()).message(errorCode.getMessage()).build();
+    }
+
+    // 이하 valid 예외 처리
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+        ex.printStackTrace();
+        return handleExceptionInternal(ex, CommonErrorCode.INVALID_INPUT_VALUE);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(BindException e, ErrorCode errorCode) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(makeErrorResponse(e, errorCode));
+    }
+
+    private ErrorResponse makeErrorResponse(BindException e, ErrorCode errorCode) {
+        List<ValidationError> validationErrorList = e.getBindingResult().getFieldErrors().stream()
+                .map(ErrorResponse.ValidationError::from).collect(Collectors.toList());
+
+        return ErrorResponse.builder().code(errorCode.name()).message(errorCode.getMessage())
+                .errors(validationErrorList).build();
     }
 }
--- src/main/java/com/guenbon/siso/dto/error/ErrorResponse.java
+++ src/main/java/com/guenbon/siso/dto/error/ErrorResponse.java
@@ -1,15 +1,36 @@
 package com.guenbon.siso.dto.error;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.FieldError;
 
-@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
 public class ErrorResponse {
     private String message;
     private String code;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private final List<ValidationError> errors;
+
+    @Getter
+    @Builder
+    @RequiredArgsConstructor
+    public static class ValidationError {
+
+        private final String field;
+        private final String message;
+
+        public static ValidationError from(final FieldError fieldError) {
+            return ValidationError.builder()
+                    .field(fieldError.getField())
+                    .message(fieldError.getDefaultMessage())
+                    .build();
+        }
+    }
 }
