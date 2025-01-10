d13cb8646c01b8ed036ec39460b35a5f6d21f0e2
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-10 16:33:26 +0900

    fix: CountCursor 검증 로직 변경
    - 모든 필드가 null 일 경우 of 메서드에서 null 반환하도록 수정

--- src/main/java/com/guenbon/siso/dto/cursor/count/CountCursor.java
+++ src/main/java/com/guenbon/siso/dto/cursor/count/CountCursor.java
@@ -5,29 +5,43 @@ import com.guenbon.siso.exception.errorCode.CursorErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Slf4j
 public class CountCursor {
 
     private String idCursor;
     private Integer countCursor;
 
     public static CountCursor of(String idCursor, Integer countCursor) {
+        if (isAllFieldNull(idCursor, countCursor)) {
+            return null;
+        }
         validateCursor(idCursor, countCursor);
         return new CountCursor(idCursor, countCursor);
     }
 
+    private static boolean isAllFieldNull(String idCursor, Integer countCursor) {
+        return idCursor == null && countCursor == null;
+    }
+
     private static void validateCursor(String idCursor, Integer countCursor) {
-        if (idCursor == null && countCursor == null) {
-            return;
-        }
         if ((idCursor == null || idCursor.isEmpty()) || countCursor == null) {
             throw new BadRequestException(CursorErrorCode.NULL_OR_EMPTY_VALUE);
         }
         if (countCursor < 0) {
             throw new BadRequestException(CursorErrorCode.NEGATIVE_VALUE);
         }
+    }
+
+    @Override
+    public String toString() {
+        return "CountCursor{" +
+                "idCursor='" + idCursor + '\'' +
+                ", countCursor=" + countCursor +
+                '}';
     }
 }
