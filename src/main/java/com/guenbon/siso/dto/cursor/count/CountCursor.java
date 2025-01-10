d219badd02fbe7e318610332e85870fb9e048da7
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-10 09:21:56 +0900

    feat: CountCursor 검증 로직 추가
    - 커서 생성시에 공통으로 필요한 검증이고, 커서의 필드에 대해서만 검증하므로 커서가 검증 책임을 갖도록 함
    - 두 필드가 모두 null인 경우는 커서를 생성하지 않는게 맞다고 생각해 추후 리팩터링하며 해당 검증은 옮길 예정

--- src/main/java/com/guenbon/siso/dto/cursor/count/CountCursor.java
+++ src/main/java/com/guenbon/siso/dto/cursor/count/CountCursor.java
@@ -1,5 +1,7 @@
 package com.guenbon.siso.dto.cursor.count;
 
+import com.guenbon.siso.exception.BadRequestException;
+import com.guenbon.siso.exception.errorCode.CursorErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,17 +10,24 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class CountCursor {
+
     private String idCursor;
     private Integer countCursor;
 
     public static CountCursor of(String idCursor, Integer countCursor) {
+        validateCursor(idCursor, countCursor);
         return new CountCursor(idCursor, countCursor);
     }
 
-    public Boolean isEmpty() {
-        if ((idCursor == null || idCursor.isEmpty()) && (countCursor == null)) {
-            return true;
+    private static void validateCursor(String idCursor, Integer countCursor) {
+        if (idCursor == null && countCursor == null) {
+            return;
         }
-        return false;
+        if ((idCursor == null || idCursor.isEmpty()) || countCursor == null) {
+            throw new BadRequestException(CursorErrorCode.NULL_OR_EMPTY_VALUE);
+        }
+        if (countCursor < 0) {
+            throw new BadRequestException(CursorErrorCode.NEGATIVE_VALUE);
+        }
     }
 }
