8e22fd1e592e83836c7e39fc6273d26fd0a15725
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-15 23:34:54 +0900

    refactor: CommonErrorCode에서 NULL_VALUE_NOT_ALLOWED 에러코드 삭제
    - 컨트롤러 - 서비스 중복 검증 삭제하며 불필요해짐
    - 그 외 null 관련 에러는 해당 도메인의 null 에러코드 사용 (명확하게 하기 위함)

--- src/main/java/com/guenbon/siso/exception/errorCode/CommonErrorCode.java
+++ src/main/java/com/guenbon/siso/exception/errorCode/CommonErrorCode.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum CommonErrorCode implements ErrorCode {
-    NULL_VALUE_NOT_ALLOWED("허용되지 않는 NULL 값입니다"),
     INVALID_INPUT_VALUE("유효하지 않은 입력 값입니다"),
     TYPE_MISMATCH("입력 값의 타입이 올바르지 않습니다"),
     INVALID_REQUEST_BODY_FORMAT("요청 본문이 올바르지 않습니다"),
