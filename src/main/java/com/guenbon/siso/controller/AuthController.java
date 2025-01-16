7e76d0aa9645899fe57fd16e1766a1657a5d3132
jd99iam <96513365+jd99iam@users.noreply.github.com>
2025-01-16 17:16:30 +0900

    refactor: Controller docs implement 삭제
    - swagger 설정 충돌로 미사용하게 되면서 삭제

--- src/main/java/com/guenbon/siso/controller/AuthController.java
+++ src/main/java/com/guenbon/siso/controller/AuthController.java
@@ -1,6 +1,5 @@
 package com.guenbon.siso.controller;
 
-import com.guenbon.siso.controller.docs.AuthControllerDocs;
 import com.guenbon.siso.dto.auth.response.LoginDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,21 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-public class AuthController implements AuthControllerDocs {
+public class AuthController {
 
-    @Override
     @GetMapping("/kakao")
     public ResponseEntity<LoginDTO> kakaoLogin(String code) {
         return null;
     }
 
-    @Override
     @PostMapping
     public ResponseEntity<LoginDTO> kakaoReissue(String refreshToken) {
         return null;
     }
 
-    @Override
     @DeleteMapping
     public ResponseEntity<Void> logOut(Long memberId) {
         return null;
--- src/main/java/com/guenbon/siso/controller/MemberController.java
+++ src/main/java/com/guenbon/siso/controller/MemberController.java
@@ -1,6 +1,5 @@
 package com.guenbon.siso.controller;
 
-import com.guenbon.siso.controller.docs.MemberControllerDocs;
 import com.guenbon.siso.dto.auth.request.SignUpDTO;
 import com.guenbon.siso.dto.auth.response.LoginDTO;
 import com.guenbon.siso.dto.member.response.MemberDetailDTO;
@@ -19,39 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/member")
-public class MemberController implements MemberControllerDocs {
+public class MemberController {
 
-    @Override
     @GetMapping("/signUp")
     public ResponseEntity<SignUpFormDTO> memberAddForm(String kakaoId) {
         return null;
     }
 
-    @Override
     @PostMapping
     public ResponseEntity<LoginDTO> memberAdd(SignUpDTO signUpDTO) {
         return null;
     }
 
-    @Override
     @GetMapping("/update")
     public ResponseEntity<MemberUpdateFormDTO> memberModifyForm(@LoginId Long loginId) {
         return null;
     }
 
-    @Override
     @PatchMapping
     public ResponseEntity<MemberUpdateDTO> memberModify(@LoginId Long loginId, MemberUpdateDTO memberUpdateRequest) {
         return null;
     }
 
-    @Override
     @DeleteMapping
     public ResponseEntity<Void> memberRemove(@LoginId Long loginId) {
         return null;
     }
 
-    @Override
     @GetMapping("/{memberId}")
     public ResponseEntity<MemberDetailDTO> memberDetail(@LoginId Long loginId, @PathVariable String memberId) {
         return null;
