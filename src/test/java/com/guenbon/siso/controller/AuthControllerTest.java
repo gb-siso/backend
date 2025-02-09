b9d9da85f109301ac18af14fd314adb8cbfd220f
jd99iam <jidamine87593@gmail.com>
2025-02-09 14:28:42 +0900

    test: AuthController 카카오 로그인 성공 테스트 작성
    -  카카오 로그인 성공 테스트 작성
    - 기존 테스트 메서드 명 재작명

--- src/test/java/com/guenbon/siso/controller/AuthControllerTest.java
+++ src/test/java/com/guenbon/siso/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.guenbon.siso.controller;
 
+import com.guenbon.siso.dto.auth.IssueTokenResult;
 import com.guenbon.siso.dto.error.ApiErrorResponse;
 import com.guenbon.siso.exception.ApiException;
 import com.guenbon.siso.exception.errorCode.KakaoApiErrorCode;
@@ -7,11 +8,14 @@ import com.guenbon.siso.service.auth.AuthApiService;
 import com.guenbon.siso.service.auth.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseCookie;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -39,7 +43,7 @@ class AuthControllerTest {
     @DisplayName("카카오 로그인 인가코드 받기 리다이렉트 시 쿼리 파라미터에 error 포함될 시 ApiErrorResponse로 응답한다")
     @ParameterizedTest
     @EnumSource(KakaoApiErrorCode.class)
-    void kakaoLogin_authCodeError_ApiErrorResponse(KakaoApiErrorCode kakaoApiErrorCode) throws Exception {
+    void kakaoLogin_redirectedWithErrorParameter_ApiErrorResponse(KakaoApiErrorCode kakaoApiErrorCode) throws Exception {
         // given
         final ApiErrorResponse expected = ApiErrorResponse.from(kakaoApiErrorCode);
 
@@ -57,7 +61,8 @@ class AuthControllerTest {
     @DisplayName("카카오 로그인 시 authenticateWithKakao 메서드에서 ApiException이 발생할 시 ApiErrorResponse로 응답한다")
     @ParameterizedTest
     @EnumSource(KakaoApiErrorCode.class)
-    void temp(KakaoApiErrorCode kakaoApiErrorCode) throws Exception {
+    void kakaoLogin_ApiExceptionAtAuthenticateWithKakao_ApiErrorResponse(KakaoApiErrorCode kakaoApiErrorCode) throws Exception {
+        // given
         final ApiErrorResponse expected = ApiErrorResponse.from(kakaoApiErrorCode);
         final String invalidKakaoAuthCode = "invalidKakaoAuthCode";
         when(authApiService.authenticateWithKakao(invalidKakaoAuthCode)).thenThrow(new ApiException(kakaoApiErrorCode));
@@ -71,5 +76,38 @@ class AuthControllerTest {
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$.message").value(expected.getMessage()))
                 .andExpect(jsonPath("$.code").value(expected.getCode()));
+    }
+
+    @DisplayName("카카오 로그인에 성공하면 LoginDTO를 응답한다")
+    @Test
+    void kakaoLogin_success_LoginDTO() throws Exception {
+        // given
+        final String validKakaoAuthCode = "validKakaoAuthCode";
+        final String dummyRefreshToken = "dummyRefreshToken";
+        final String refreshTokenCookie = ResponseCookie.from("refreshToken", dummyRefreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .sameSite(Cookie.SameSite.NONE.attributeValue())
+                .build().toString();
+        final IssueTokenResult expected = IssueTokenResult.builder()
+                .refreshTokenCookie(refreshTokenCookie)
+                .accessToken("accessToken")
+                .image("image")
+                .nickname("nickname").build();
+
+        when(authApiService.authenticateWithKakao(validKakaoAuthCode)).thenReturn(expected);
+
+        // when then
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + LOGIN_KAKAO_PATH)
+                        .param(CODE, validKakaoAuthCode) // errorCode 대신 getCode() 사용
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful()) // HTTP 상태 코드 비교
+                .andExpect(content().contentType("application/json"))
+                .andExpect(cookie().value("refreshToken", dummyRefreshToken))
+                .andExpect(jsonPath("$.nickname").value(expected.getNickname()))
+                .andExpect(jsonPath("$.imageUrl").value(expected.getImage()))
+                .andExpect(jsonPath("$.accessToken").value(expected.getAccessToken()));
     }
 }
