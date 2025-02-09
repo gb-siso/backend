29fe01ed9734a258ce58e8ed6c17a37a993bc898
jd99iam <jidamine87593@gmail.com>
2025-02-09 13:39:20 +0900

    test: 카카오 로그인 인가코드 받은 후 authenticateWithKakao 에서 예외 발생 실패 테스트 작성

--- src/test/java/com/guenbon/siso/controller/AuthControllerTest.java
+++ src/test/java/com/guenbon/siso/controller/AuthControllerTest.java
@@ -1,36 +1,73 @@
 package com.guenbon.siso.controller;
 
 import com.guenbon.siso.dto.error.ApiErrorResponse;
+import com.guenbon.siso.exception.ApiException;
 import com.guenbon.siso.exception.errorCode.KakaoApiErrorCode;
+import com.guenbon.siso.service.auth.AuthApiService;
+import com.guenbon.siso.service.auth.JwtTokenProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-class AuthControllerTest extends ControllerTest {
+@WebMvcTest(controllers = {AuthController.class})
+@Slf4j
+class AuthControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+    @MockitoBean
+    protected AuthApiService authApiService;
+    @MockitoBean
+    protected JwtTokenProvider jwtTokenProvider;
 
     public static final String BASE_URL = "/api/v1/auth";
+    public static final String CODE = "code";
+    public static final String ERROR = "error";
+    public static final String LOGIN_KAKAO_PATH = "/login/kakao";
 
+    @DisplayName("카카오 로그인 인가코드 받기 리다이렉트 시 쿼리 파라미터에 error 포함될 시 ApiErrorResponse로 응답한다")
     @ParameterizedTest
-    @ValueSource(strings = {
-            "KOE001", "KOE002", "KOE003", "KOE004", "KOE005", "KOE006", "KOE007", "KOE008",
-            "KOE009", "KOE010", "KOE101", "KOE102", "KOE201", "KOE202", "KOE203", "KOE204",
-            "KOE205", "KOE207", "KOE303", "KOE310", "KOE319", "KOE320", "KOE322", "KOE406",
-            "KOE501", "KOE602", "KOE903", "KOE911", "KOE9798"
-    })
-    void kakaoLogin_authCodeError_ApiException(String errorCode) throws Exception {
-
-        final KakaoApiErrorCode kakaoApiErrorCode = KakaoApiErrorCode.from(errorCode);
+    @EnumSource(KakaoApiErrorCode.class)
+    void kakaoLogin_authCodeError_ApiErrorResponse(KakaoApiErrorCode kakaoApiErrorCode) throws Exception {
+        // given
         final ApiErrorResponse expected = ApiErrorResponse.from(kakaoApiErrorCode);
+
         // when then
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/login/kakao")
-                        .param("error", errorCode)
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + LOGIN_KAKAO_PATH)
+                        .param(ERROR, kakaoApiErrorCode.getCode()) // errorCode 대신 getCode() 사용
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().is(kakaoApiErrorCode.getHttpStatus().value()))
+                .andExpect(status().is(kakaoApiErrorCode.getHttpStatus().value())) // HTTP 상태 코드 비교
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.message").value(expected.getMessage()))
+                .andExpect(jsonPath("$.code").value(expected.getCode()));
+    }
+
+    @DisplayName("카카오 로그인 시 authenticateWithKakao 메서드에서 ApiException이 발생할 시 ApiErrorResponse로 응답한다")
+    @ParameterizedTest
+    @EnumSource(KakaoApiErrorCode.class)
+    void temp(KakaoApiErrorCode kakaoApiErrorCode) throws Exception {
+        final ApiErrorResponse expected = ApiErrorResponse.from(kakaoApiErrorCode);
+        final String invalidKakaoAuthCode = "invalidKakaoAuthCode";
+        when(authApiService.authenticateWithKakao(invalidKakaoAuthCode)).thenThrow(new ApiException(kakaoApiErrorCode));
+
+        // when then
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + LOGIN_KAKAO_PATH)
+                        .param(CODE, invalidKakaoAuthCode) // errorCode 대신 getCode() 사용
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(kakaoApiErrorCode.getHttpStatus().value())) // HTTP 상태 코드 비교
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$.message").value(expected.getMessage()))
                 .andExpect(jsonPath("$.code").value(expected.getCode()));
