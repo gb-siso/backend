48e51f51041250ee14e7655002f72383f98f0fce
jd99iam <jidamine87593@gmail.com>
2025-02-10 20:40:24 +0900

    chore: cors 설정 추가

--- src/main/java/com/guenbon/siso/config/WebConfig.java
+++ src/main/java/com/guenbon/siso/config/WebConfig.java
@@ -3,12 +3,14 @@ package com.guenbon.siso.config;
 import com.guenbon.siso.argumentresolver.CustomPageableResolver;
 import com.guenbon.siso.argumentresolver.LoginIdArgumentResolver;
 import com.guenbon.siso.interceptor.AuthInterceptor;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,5 +30,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .excludePathPatterns();
+    }
+
+    // ✅ CORS 설정 추가
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 엔드포인트에서 CORS 허용
+                .allowedOrigins("*") // 모든 도메인에서 요청 가능
+                .allowedMethods("GET", "POST", "DELETE", "PATCH") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 요청 헤더 허용
+                .allowCredentials(false); // 인증 정보 포함 여부 (true 사용 시 특정 origin 필요)
     }
 }
