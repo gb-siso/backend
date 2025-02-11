7cdb3f6ca50fb131b6b660d4973ff690b0ac066a
jd99iam <jidamine87593@gmail.com>
2025-02-11 22:17:53 +0900

    test: 네이버 로그인 컨트롤러 테스트 작성
    - 실패 테스트 및 성공 테스트 작성

--- src/test/java/com/guenbon/siso/controller/AuthControllerTest.java
+++ src/test/java/com/guenbon/siso/controller/AuthControllerTest.java
@@ -4,6 +4,7 @@ import com.guenbon.siso.dto.auth.IssueTokenResult;
 import com.guenbon.siso.dto.error.ApiErrorResponse;
 import com.guenbon.siso.exception.ApiException;
 import com.guenbon.siso.exception.errorCode.KakaoApiErrorCode;
+import com.guenbon.siso.exception.errorCode.NaverApiErrorCode;
 import com.guenbon.siso.service.auth.AuthApiService;
 import com.guenbon.siso.service.auth.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
@@ -39,6 +40,7 @@ class AuthControllerTest {
     public static final String CODE = "code";
     public static final String ERROR = "error";
     public static final String LOGIN_KAKAO_PATH = "/login/kakao";
+    public static final String LOGIN_NAVER_PATH = "/login/naver";
 
     @DisplayName("카카오 로그인 인가코드 받기 리다이렉트 시 쿼리 파라미터에 error 포함될 시 ApiErrorResponse로 응답한다")
     @ParameterizedTest
@@ -101,6 +103,76 @@ class AuthControllerTest {
         // when then
         mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + LOGIN_KAKAO_PATH)
                         .param(CODE, validKakaoAuthCode) // errorCode 대신 getCode() 사용
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful()) // HTTP 상태 코드 비교
+                .andExpect(content().contentType("application/json"))
+                .andExpect(cookie().value("refreshToken", dummyRefreshToken))
+                .andExpect(jsonPath("$.nickname").value(expected.getNickname()))
+                .andExpect(jsonPath("$.imageUrl").value(expected.getImage()))
+                .andExpect(jsonPath("$.accessToken").value(expected.getAccessToken()));
+    }
+
+    @DisplayName("네이버 로그인 인가코드 받기 리다이렉트 시 쿼리 파라미터에 error 포함될 시 ApiErrorResponse로 응답한다")
+    @ParameterizedTest
+    @EnumSource(NaverApiErrorCode.class)
+    void naverLogin_redirectedWithErrorParameter_ApiErrorResponse(NaverApiErrorCode naverApiErrorCode) throws Exception {
+        // given
+        final ApiErrorResponse expected = ApiErrorResponse.from(naverApiErrorCode);
+        // when then
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + LOGIN_NAVER_PATH)
+                        .param(ERROR, naverApiErrorCode.getCode()) // errorCode 대신 getCode() 사용
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(naverApiErrorCode.getHttpStatus().value())) // HTTP 상태 코드 비교
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.message").value(expected.getMessage()))
+                .andExpect(jsonPath("$.code").value(expected.getCode()));
+    }
+
+    @DisplayName("카카오 로그인 시 authenticateWithNaver 메서드에서 ApiException이 발생할 시 ApiErrorResponse로 응답한다")
+    @ParameterizedTest
+    @EnumSource(NaverApiErrorCode.class)
+    void naverLogin_ApiExceptionAtAuthenticateWithNaver_ApiErrorResponse(NaverApiErrorCode naverApiErrorCode) throws Exception {
+        // given
+        final ApiErrorResponse expected = ApiErrorResponse.from(naverApiErrorCode);
+        final String invalidNaverAuthCode = "invalidNaverAuthCode";
+        when(authApiService.authenticateWithNaver(invalidNaverAuthCode)).thenThrow(new ApiException(naverApiErrorCode));
+
+        // when then
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + LOGIN_NAVER_PATH)
+                        .param(CODE, invalidNaverAuthCode) // errorCode 대신 getCode() 사용
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(naverApiErrorCode.getHttpStatus().value())) // HTTP 상태 코드 비교
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.message").value(expected.getMessage()))
+                .andExpect(jsonPath("$.code").value(expected.getCode()));
+    }
+
+    @DisplayName("네이버 로그인에 성공하면 LoginDTO를 응답한다")
+    @Test
+    void naverLogin_success_LoginDTO() throws Exception {
+        // given
+        final String validNaverAuthCode = "validNaverAuthCode";
+        final String dummyRefreshToken = "dummyRefreshToken";
+        final String refreshTokenCookie = ResponseCookie.from("refreshToken", dummyRefreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .sameSite(Cookie.SameSite.NONE.attributeValue())
+                .build().toString();
+        final IssueTokenResult expected = IssueTokenResult.builder()
+                .refreshTokenCookie(refreshTokenCookie)
+                .accessToken("accessToken")
+                .image("image")
+                .nickname("nickname").build();
+
+        when(authApiService.authenticateWithKakao(validNaverAuthCode)).thenReturn(expected);
+
+        // when then
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + LOGIN_NAVER_PATH)
+                        .param(CODE, validNaverAuthCode) // errorCode 대신 getCode() 사용
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful()) // HTTP 상태 코드 비교
