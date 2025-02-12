a9830341cdf81d20789d3d35acea30bf1acd6e4e
jd99iam <jidamine87593@gmail.com>
2025-02-12 17:36:54 +0900

    chore: ddl 수정
    - naver_id 컬럼 varchar(255)로 수정

--- src/main/resources/sql_script/ddl_0208.sql
+++ src/main/resources/sql_script/ddl_0208.sql
@@ -10,7 +10,7 @@ create table dislike (congressman_id bigint, id bigint not null auto_increment, 
 
 create table `like` (congressman_id bigint, created_date datetime(6), id bigint not null auto_increment, member_id bigint, modified_date datetime(6), rating_id bigint, dtype varchar(31) not null, primary key (id)) engine=InnoDB;
 
-create table member (created_date datetime(6), id bigint not null auto_increment, kakao_id bigint unique, naver_id bigint unique, modified_date datetime(6), image_url varchar(255), nickname varchar(255), refresh_token varchar(255), primary key (id)) engine=InnoDB;
+create table member (created_date datetime(6), id bigint not null auto_increment, kakao_id bigint unique, naver_id varchar(255) unique, modified_date datetime(6), image_url varchar(255), nickname varchar(255), refresh_token varchar(255), primary key (id)) engine=InnoDB;
 
 create table rating (rate float(53), congressman_id bigint, created_date datetime(6), id bigint not null auto_increment, member_id bigint, modified_date datetime(6), content varchar(255), primary key (id)) engine=InnoDB;
 
