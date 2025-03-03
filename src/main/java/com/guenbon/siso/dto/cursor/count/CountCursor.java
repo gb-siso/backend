2ac32d01bb7b3e49920cb9f0aa3a2022e85e6fc8
jd99iam <jidamine87593@gmail.com>
2025-03-03 22:36:39 +0900

    refcator: CountCursor에 `@JsonIgnore` 추가

--- src/main/java/com/guenbon/siso/dto/cursor/count/CountCursor.java
+++ src/main/java/com/guenbon/siso/dto/cursor/count/CountCursor.java
@@ -1,5 +1,6 @@
 package com.guenbon.siso.dto.cursor.count;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
@@ -19,6 +20,7 @@ public class CountCursor {
     @Min(value = 0, message = "countCursor는 0 이상이어야 합니다.")
     private Integer countCursor;
 
+    @JsonIgnore
     @AssertTrue(message = "일부 커서만 유효할 수 없습니다.")
     public boolean isCursorValid() {
         if (isAllFieldInvalid()) { // 모든 필드 inValid
@@ -28,19 +30,22 @@ public class CountCursor {
         return !isSomeFieldInvalid();// 모든 필드 valid
     }
 
+    @JsonIgnore
     private boolean isSomeFieldInvalid() {
         return !isCountCursorValid() || !isIdCursorValid();
     }
 
+    @JsonIgnore
     public boolean isAllFieldInvalid() {
         return !isCountCursorValid() && !isIdCursorValid();
     }
 
-
+    @JsonIgnore
     public boolean isIdCursorValid() {
         return idCursor != null && !idCursor.isBlank() && !idCursor.equals("null");
     }
 
+    @JsonIgnore
     public boolean isCountCursorValid() {
         return countCursor != null && countCursor >= 0;
     }
