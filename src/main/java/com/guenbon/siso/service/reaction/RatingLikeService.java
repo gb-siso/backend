00dcf13d334ea049653fc059d5bf5eabe9c75391
jd99iam <jidamine87593@gmail.com>
2025-03-05 22:14:24 +0900

    feat: 평가 좋아요 작성 요청 컨트롤러 테스트 통과하도록 로직 작성
    컨트롤러 로직 작성 및 필요 클래스 작성
    - RatingLikeErrorCode 작성
    - RatingLikeService 작성 (컨트롤러 테스트만 통과하면 되기 때문에 메서드 시그니처만 작성)
    - RatingReactionDTO 작성
    - ReactionController 작성
    - ReactionStatus 작성

---
+++ src/main/java/com/guenbon/siso/service/reaction/RatingLikeService.java
@@ -0,0 +1,11 @@
+package com.guenbon.siso.service.reaction;
+
+import com.guenbon.siso.dto.reaction.response.RatingReactionDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingLikeService {
+    public RatingReactionDTO create(String encryptedRatingId, Long memberId) {
+        return null;
+    }
+}
