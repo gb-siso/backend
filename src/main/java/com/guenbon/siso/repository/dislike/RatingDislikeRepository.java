0f4beaeeaaff2be6276144df4f9bce09c8f39735
jd99iam <jidamine87593@gmail.com>
2025-03-10 23:20:16 +0900

    feat: RatingDisLikeServiceTest 통과하도록 RatingDisLikeService 작성

--- src/main/java/com/guenbon/siso/repository/dislike/RatingDislikeRepository.java
+++ src/main/java/com/guenbon/siso/repository/dislike/RatingDislikeRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RatingDislikeRepository extends JpaRepository<RatingDislike, Long>, QuerydslRatingDislikeRepository {
+
     Optional<RatingDislike> findByRatingIdAndMemberId(Long ratingId, Long memberId);
 
+    boolean existsByRatingIdAndMemberId(Long ratingId, Long memberId);
 }
