d2cdf64b895b0b97bb91936075ae2a095f2d50ab
jidamine87593 <jidamine87593@gmail.com>
2025-07-14 22:09:58 +0900

    chore: jenkins test

--- src/main/java/com/guenbon/siso/SisoApplication.java
+++ src/main/java/com/guenbon/siso/SisoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling // 스케줄링 활성화
 @SpringBootApplication
 public class SisoApplication {
-// test jenkins
+// test jenkins 2
     public static void main(String[] args) {
         SpringApplication.run(SisoApplication.class, args);
     }
