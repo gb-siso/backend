3db863f761b8093c167a871f716967daaf9207c2
jidamine87593 <jidamine87593@gmail.com>
2025-07-20 20:52:19 +0900

    chore: 젠킨스 동작 확인 2

--- src/main/java/com/guenbon/siso/SisoApplication.java
+++ src/main/java/com/guenbon/siso/SisoApplication.java
@@ -9,20 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling // 스케줄링 활성화
 @SpringBootApplication
 public class SisoApplication {
-
-    static Logger log = LoggerFactory.getLogger(SisoApplication.class);
-
     public static void main(String[] args) {
-
-        System.out.println("test jenkins work");
-
-        log.info("test jenkins work");
-        log.info("test jenkins work");
-        log.info("test jenkins work");
-        log.info("test jenkins work");
-        log.info("test jenkins work");
-        log.info("test jenkins work");
-
         SpringApplication.run(SisoApplication.class, args);
     }
 
