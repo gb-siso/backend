634cfd073574fa0e57f56eb043d22dad3718590e
jidamine87593 <jidamine87593@gmail.com>
2025-07-22 23:52:56 +0900

    chore: 최종 수정

--- src/main/java/com/guenbon/siso/SisoApplication.java
+++ src/main/java/com/guenbon/siso/SisoApplication.java
@@ -1,15 +1,18 @@
 package com.guenbon.siso;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Slf4j
 @EnableScheduling // 스케줄링 활성화
 @SpringBootApplication
 public class SisoApplication {
     public static void main(String[] args) {
+        log.info("jenkins build test");
         SpringApplication.run(SisoApplication.class, args);
     }
 
